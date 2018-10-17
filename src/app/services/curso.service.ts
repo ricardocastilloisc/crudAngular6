70c882806774f3441e2bb02c1ba39630845acce7
Ricardo Castillo <ricardocastilloisc@gmail.com>
2018-10-17 12:42:16 -0500

    aplicacion parte 2

--- src/app/services/curso.service.ts
+++ src/app/services/curso.service.ts
@@ -8,14 +8,25 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class CursoService {
+
+  //inicializaciones de la base de datos
   cursosCollection: AngularFirestoreCollection <CursoInterface>;
   cursos: Observable<CursoInterface[]>;
   cursoDoc: AngularFirestoreDocument<CursoInterface>;
+
+  //constructor donde afs es firebase
   constructor(public afs: AngularFirestore) {
+    ///no olvidar instalar npm i --save firebase angularfire2
+    //estamos diciendo que cursos
+    //de la interface que ya esta hecha
+    //de la tabla 'cursos' que esta en firebase
+    //acceder a los datos
+    //con valueChanges
       this.cursos = afs.collection('cursos').valueChanges();
   }
 
   getCursos() {
+    //visualisamos lo que hay en la base de datos general
     return this.cursos;
   }
 
